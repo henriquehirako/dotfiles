b242ae826984f334571243edb09d2ba52930c148
Henrique Hirako <me@henriquehirako.com>
2016-05-23 14:18:21 -0300

    youcompleteme requisites

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 ln -s vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# Must have cmake and etc... install automake gcc gcc-c++ kernel-devel cmake
+# and this: install python-devel python3-devel
+/home/hirako/.vim/bundle/youcompleteme/install.py --tern-completer
